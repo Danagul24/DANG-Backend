0b04cadcdb1daa991d5c58796d9ce28439d29de5
Danagul24 <danagul.24@mail.ru>
2023-06-05 16:27:13 +0600

    allowed hosts

--- dang/dang/settings.py
+++ dang/dang/settings.py
@@ -31,7 +31,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     'e3d5-87-255-201-182.ngrok-free.app',
-    'http://127.0.0.1:8000/'
+    'http://127.0.0.1:8000/',
+    'https://dang-backend-i48hecgxl-danagul24.vercel.app/',
+    'dang-backend.vercel.app',
+
 ]
 
 # Application definition
