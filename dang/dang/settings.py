41cb0c8904fc02054db21d6b5ed60589b15c8099
Danagul24 <danagul.24@mail.ru>
2023-06-06 16:09:49 +0600

    vercel

--- dang/dang/settings.py
+++ dang/dang/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-0tf0l)m=&pb0n$%6=x^gv1c6+zl-da-@5^a$yys#j9oj2d8b_l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1']
 
 # Application definition
 
